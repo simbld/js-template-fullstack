b49902042a55a2ec0e356b03ede0fab5747b7914
SIMON BULLADO <s_bullado51@hotmail.com>
2023-02-02 01:28:54 +0100

    Add assets pitures And create table in database

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,49 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE
+  asset (
+    id int (11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    quantity varchar(255) NOT NULL,
+    worth varchar(255) NOT NULL,
+    img_asset varchar(255) NOT NULL
+  ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+INSERT INTO
+  asset (title, worth, quantity, img_asset)
+VALUES
+  (
+    'stables',
+    42,
+    6000000,
+    '/home/simon/CHECKPOINT/checkpoint-4-Kingdom-of-Colchis/frontend/src/pic/stables.png'
+  ),
+  (
+    'castles',
+    4,
+    24000000,
+    '/home/simon/CHECKPOINT/checkpoint-4-Kingdom-of-Colchis/frontend/src/pic/castles.jpg'
+  ),
+  (
+    'guards',
+    10000,
+    10000000,
+    '/home/simon/CHECKPOINT/checkpoint-4-Kingdom-of-Colchis/frontend/src/pic/guards.png'
+  ),
+  ('wagons', 18, 4000, 'pic/wagons.png'),
+  (
+    'livestocks',
+    51942,
+    4000000,
+    '/home/simon/CHECKPOINT/checkpoint-4-Kingdom-of-Colchis/frontend/src/pic/livestocks.jpg'
+  ),
+  (
+    'armors',
+    997000,
+    11000000,
+    '/home/simon/CHECKPOINT/checkpoint-4-Kingdom-of-Colchis/frontend/src/pic/armors.png'
+  ),
+  (
+    'canons',
+    99,
+    110000000,
+    '/home/simon/CHECKPOINT/checkpoint-4-Kingdom-of-Colchis/frontend/src/pic/canons.png'
+  );
